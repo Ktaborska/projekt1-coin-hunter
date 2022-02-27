8d5161888fd4c47e5b84759f52812c30ea7187e0
Kateřina Táborská <ktaborska92@gmail.com>
2022-02-27 21:53:46 +0100

    adding function for moving Hunter

--- main.js
+++ main.js
@@ -7,3 +7,30 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+
+function movingHunter(event) {
+
+	console.log(event)
+
+	console.log(event.key)
+
+	let key = event.key
+	let panacek = document.querySelector('#panacek');
+
+	if (key === "ArrowDown"){
+		panacek.style.left = '25px';
+	}
+	if (key === "ArrowUp"){
+		panacek.style.left = '25px';
+	}
+	if (key === "ArrowLeft"){
+		panacek.style.left = '25px';
+	}
+	if (key === "ArrowRight"){
+		panacek.style.left = '25px';
+	}
+}
+
+// na celym dokumentu pri stisku jakekoliv klavsy se zavola moje funkce moviengHunter
+document.addEventListener("keydown", movingHunter);
+
