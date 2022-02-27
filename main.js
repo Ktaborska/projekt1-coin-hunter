4ef639f00da2ace96e2227a238b50531188475ca
Kateřina Táborská <ktaborska92@gmail.com>
2022-02-27 23:05:54 +0100

    changing image src attribute

--- main.js
+++ main.js
@@ -16,18 +16,22 @@ function movingHunter(event) {
 	let panacek = document.querySelector('#panacek');
 
 	if (key === "ArrowDown"){	
+		panacek.src = 'obrazky/panacek.png';
 		let moveDown = parseInt(window.getComputedStyle(panacek).top);
 		panacek.style.top = (moveDown + 20) +'px';
 	}
 	if (key === "ArrowUp"){
+		panacek.src = 'obrazky/panacek-nahoru.png';
 		let moveUp = parseInt(window.getComputedStyle(panacek).top);
 		panacek.style.top = (moveUp - 20) +'px';
 	}
 	if (key === "ArrowLeft"){
+		panacek.src = 'obrazky/panacek-vlevo.png';
 		let moveLeft = parseInt(window.getComputedStyle(panacek).left);
 		panacek.style.left = (moveLeft - 20) +'px';
 	}
 	if (key === "ArrowRight"){
+		panacek.src = 'obrazky/panacek-vpravo.png';
 		let moveRight = parseInt(window.getComputedStyle(panacek).left);
 		panacek.style.left = (moveRight + 20) +'px';
 	}
