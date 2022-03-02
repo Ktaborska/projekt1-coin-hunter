64bc7998631ef186cc754fc9ef6ba5e2b38fe407
Kateřina Táborská <ktaborska92@gmail.com>
2022-03-02 21:58:50 +0100

    nové řešení podle zadání + adding function for coin movement

--- main.js
+++ main.js
@@ -5,80 +5,13 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 }
 */
 
-
-// sem začni psát svůj program
-
-console.log(typeof window.innerWidth)
-console.log(window.innerWidth + 20)
-
-let height = window.innerHeight;
-let width = window.innerWidth;
-const movement = 20;
-
-function movingHunter(event) {
-
-	let key = event.key
-	let panacek = document.querySelector('#panacek');
-	
-
-	if (key === "ArrowDown"){	
-		panacek.src = 'obrazky/panacek.png';
-		let moveDown = parseInt(window.getComputedStyle(panacek).top);
-
-		if((moveDown + 64 + movement) > height){
-			console.log('stop')
-		}else{
-			panacek.style.top = (moveDown + movement) +'px';
-		}
-	}
-
-	if (key === "ArrowUp"){
-		panacek.src = 'obrazky/panacek-nahoru.png';
-		let moveUp = parseInt(window.getComputedStyle(panacek).top);
-	
-	//když by následující pohyb šel do záporu, tak mu nastavím výchozí pozici top=0
-
-		if((moveUp - movement) < 0){
-			panacek.style.top = '0px';
-		}else{
-			panacek.style.top = (moveUp - movement) +'px';
-		}
-	}
-
-	if (key === "ArrowLeft"){
-		panacek.src = 'obrazky/panacek-vlevo.png';
-		let moveLeft = parseInt(window.getComputedStyle(panacek).left);
-
-	//když by následující pohyb šel do záporu, tak mu nastavím výchozí pozici left=0
-
-		if((moveLeft -movement) < 0){
-			panacek.style.left = '0px';
-		}else{
-			panacek.style.left = (moveLeft - movement) +'px';
-		}
-	}
-	
-	if (key === "ArrowRight"){
-		panacek.src = 'obrazky/panacek-vpravo.png';
-		let moveRight = parseInt(window.getComputedStyle(panacek).left);
-	
-
-		if((moveRight + 70 + movement) > width){
-			console.log('stop')
-		}else{
-			panacek.style.left = (moveRight + movement) +'px';
-		}
-	}
-}
-
-// na celem dokumentu se pri stisku jakekoliv klavesy zavola funkce movingHunter
-
 document.addEventListener("keydown", movingHunter);
 
-
-/*
 let x = 0;
 let y = 0;
+const movement = 20;
+let height = window.innerHeight;
+let width = window.innerWidth;
 
 function movingHunter(event) {
 
@@ -86,11 +19,61 @@ let key = event.key
 let panacek = document.querySelector('#panacek');
 
 	if (key === "ArrowDown"){
-		x=x;
-		y=y+30;
-		panacek.style.top = (y +30) +'px';
+		if(y+20+64 < height){
+			y = y+20;
+			panacek.style.top = y +'px';
+		}
 	}
 
+	if (key === "ArrowUp"){
+		if(y-20 < 0){
+			panacek.style.top='0px'
+		}else{
+			y = y-20;
+			panacek.style.top = y +'px';
+			}
+	}
+
+	if (key === "ArrowLeft"){
+		if(x-20 < 0){
+			panacek.style.left='0px'
+		}else{
+			x = x-20;
+			panacek.style.left = x +'px';
+		}
+	}
+
+	if (key === "ArrowRight"){
+		if((x+20+70) < width){
+			x = x+20;
+			panacek.style.left = x +'px';
+		}	
+	}
 }
 
-document.addEventListener("keydown", movingHunter);*/
+let xCoin = 0;
+let yCoin = 0;
+
+function movingCoin(){
+	
+	let coin = document.querySelector('#mince');
+	xCoin = Math.floor(Math.random() * width);
+	yCoin = Math.floor(Math.random() * height);
+
+	if(xCoin + 36 > width){
+		xCoin = xCoin - 36;
+	}
+
+	if(yCoin + 36 > height){
+		yCoin = yCoin - 36;
+	}
+
+	coin.style.left = xCoin +'px';
+	coin.style.top = yCoin + 'px';
+}
+
+
+window.addEventListener('load', movingCoin);
+
+
+
